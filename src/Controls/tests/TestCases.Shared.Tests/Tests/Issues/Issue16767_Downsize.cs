84dbb748c31dd3166f6b58e6427c931298baf1fa
HarishwaranVijayakumar <harishwaran.vijayakumar@syncfusion.com>
2025-04-24 14:38:10 +0530

    Added Testcase

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue16767_Downsize.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.Maui.TestCases.Tests.Tests.Issues
+{
+    internal class Issue16767_Downsize
+    {
+    }
+}
