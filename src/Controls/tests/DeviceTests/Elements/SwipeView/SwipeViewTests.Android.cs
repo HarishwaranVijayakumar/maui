be05f4f8eb84d5facf097b08194b6c516eabc897
HarishwaranVijayakumar <harishwaran.vijayakumar@syncfusion.com>
2026-09-24 11:05:54 +0530

    Add fix

--- src/Controls/tests/DeviceTests/Elements/SwipeView/SwipeViewTests.Android.cs
+++ src/Controls/tests/DeviceTests/Elements/SwipeView/SwipeViewTests.Android.cs
@@ -445,8 +445,8 @@ namespace Microsoft.Maui.DeviceTests
 				var actionView = Assert.Single(
 					Enumerable.Range(0, platformView.ChildCount)
 						.Select(platformView.GetChildAt)
-						.OfType<ALinearLayoutCompat>()
-						.Where(view => view.ChildCount > 0));
+						.OfType<ALinearLayoutCompat>(),
+					view => view.ChildCount > 0);
 				var swipeButton = Assert.IsAssignableFrom<ATextView>(actionView.GetChildAt(0));
 
 				await AssertEventually(() =>
